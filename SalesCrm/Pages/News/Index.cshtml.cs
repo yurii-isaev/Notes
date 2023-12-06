dabde88f3079222bd9307c88a0cf3eafc9f86f4c
yurii-isaev <devhouse27@gmail.com>
2023-12-06 20:23:14 +0200

    feat: manager news page pagination

--- SalesCrm/Pages/News/Index.cshtml.cs
+++ SalesCrm/Pages/News/Index.cshtml.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using NToastNotify;
 using SalesCrm.Controllers.ViewModels;
 using SalesCrm.Services.Contracts.Services;
+using SalesCrm.Services.Input;
 
 namespace SalesCrm.Pages.News
 {
@@ -24,21 +25,34 @@ namespace SalesCrm.Pages.News
         }
 
         [BindProperty(Name = "newsId")] public Guid NewsId { get; set; }
-
         [BindProperty] public IEnumerable<NewsViewModel> NewsList { get; set; } = null!;
+        
+        [BindProperty] public int TotalItemsCount { get; set; }
+        [BindProperty] public int PageSize { get; set; }
+        [BindProperty] public int PageNumber { get; set; }
+        [BindProperty] public int TotalPages { get; set; }
+        [BindProperty] public bool IsPreviousPageAvailable { get; set; }
+        [BindProperty] public bool IsNextPageAvailable { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string keyword, int pageNumber = 1, int pageSize = 5)
         {
-            try
-            {
-                var news = await _newsService.GetOnlyActiveNewsAsync();
-                var viewModel = _mapper.Map<IEnumerable<NewsViewModel>>(news);
-                NewsList = viewModel;
-            }
-            catch (Exception ex)
-            {
-                RedirectToPage("Error", new { errorMessage = ex.Message });
-            }
+            IEnumerable<NewsDto> news = await _newsService.GetOnlyActiveNewsAsync(keyword);
+            IEnumerable<NewsDto> newsList = news.ToList();
+            List<NewsViewModel> list = newsList
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .Select(item => _mapper.Map<NewsViewModel>(item))
+                .ToList();
+        
+            NewsList = list;
+        
+            // Set values for pagination properties
+            TotalItemsCount = newsList.Count();
+            PageSize = pageSize;
+            PageNumber = pageNumber;
+            TotalPages = (int)Math.Ceiling((double)TotalItemsCount / PageSize);
+            IsPreviousPageAvailable = PageNumber > 1;
+            IsNextPageAvailable = PageNumber < TotalPages;
         }
 
         [Authorize(Roles = "Manager")]
